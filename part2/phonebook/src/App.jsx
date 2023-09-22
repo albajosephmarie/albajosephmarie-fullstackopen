181e7e2f2853a484674f17021645b2fc6fe52061
Joseph Marie Alba <albajosephmarie@gmail.com>
2023-09-22 05:50:08 -0400

    exercises-2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,11 +6,15 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault();
-    const newPerson = {
-      name: newName,
-    };
-    setPersons(persons.concat(newPerson));
-    setNewName("")
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      const newPerson = {
+        name: newName,
+      };
+      setPersons(persons.concat(newPerson));
+    }
+    setNewName("");
   };
 
   const handleNameChange = (event) => {
